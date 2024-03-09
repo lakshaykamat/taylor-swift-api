1f4e39b1f6badbd65d74dd2238579b9fb00ae90c
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-03-09 14:46:05 +0530

    Feature/imporve performance (#3)
    * vercel config add
    
    * Change node 14.x to 20.x
    
    * 18
    
    * appjs
    
    * Add Nodemon
    
    * Code Refactor
    
    * Add HttpStatus Codes
    
    * Minor Change in Models
    
    - title should be unique
    - album name is now required in song model
    - name should be unique
    
    * Create Get Song By Album Route
    
    * Minor Refactor
    
    * Admin routes are fixed

--- routes/song.js
+++ routes/song.js
@@ -4,12 +4,17 @@ const {
   newSong,
   searchSongs,
   getRandomSong,
+  getSong,
+  getSongsByAlbum,
 } = require("../controllers/songController");
 const router = express.Router();
 
 router.route("/").get(getAllSongs);
+router.route("/album/:albumName").get(getSongsByAlbum);
 router.route("/random").get(getRandomSong);
 router.route("/search").get(searchSongs);
 router.route("/new").post(newSong);
+
+router.route("/:songName").get(getSong);
 
 module.exports = router;
